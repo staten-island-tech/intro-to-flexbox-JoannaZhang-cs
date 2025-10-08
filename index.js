495da1f9c9cbc44e3f64a40fa3ea43312c3617f3
JoannaZhang-cs <jzsunset10@gmail.com>
2025-10-08 12:32:23 -0400

    10/8

---
+++ index.js
@@ -0,0 +1,14 @@
+const rackets = [
+    {
+        name: "Babolot Pure Strike",
+        price: 59,
+        inStock: true,
+        brand: "Babolot",
+        src : "https://tse3.mm.bing.net/th/id/OIP.xzRQubpnNOIpMbKdyBxbjwAAAA?r=0&rs=1&pid=ImgDetMain&o=7&rm=3",
+        alt : "Babolat Pure Drive"
+    },
+
+];
+
+sneakers.forEach((rackets) => makeCard(rackets));
+function makeCard(rackets)
