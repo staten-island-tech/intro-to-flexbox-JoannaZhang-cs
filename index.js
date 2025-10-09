e8b023e0f71bace0a9c4f410ad70e0a1306c2bb4
JoannaZhang-cs <jzsunset10@gmail.com>
2025-10-09 11:53:26 -0400

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,115 @@ const rackets = [
         alt : "Babolat Pure Drive"
     },
 
+        {
+        name: "Babolot Pure Strike",
+        price: 59,
+        inStock: true,
+        brand: "Babolot",
+        src : "https://tse3.mm.bing.net/th/id/OIP.xzRQubpnNOIpMbKdyBxbjwAAAA?r=0&rs=1&pid=ImgDetMain&o=7&rm=3",
+        alt : "Babolat Pure Drive"
+    },
+    
+        {
+        name: "Babolot Pure Strike",
+        price: 59,
+        inStock: true,
+        brand: "Babolot",
+        src : "https://tse3.mm.bing.net/th/id/OIP.xzRQubpnNOIpMbKdyBxbjwAAAA?r=0&rs=1&pid=ImgDetMain&o=7&rm=3",
+        alt : "Babolat Pure Drive"
+    },
+
+        {
+        name: "Babolot Pure Strike",
+        price: 59,
+        inStock: true,
+        brand: "Babolot",
+        src : "https://tse3.mm.bing.net/th/id/OIP.xzRQubpnNOIpMbKdyBxbjwAAAA?r=0&rs=1&pid=ImgDetMain&o=7&rm=3",
+        alt : "Babolat Pure Drive"
+    },
+
+        {
+        name: "Babolot Pure Strike",
+        price: 59,
+        inStock: true,
+        brand: "Babolot",
+        src : "https://tse3.mm.bing.net/th/id/OIP.xzRQubpnNOIpMbKdyBxbjwAAAA?r=0&rs=1&pid=ImgDetMain&o=7&rm=3",
+        alt : "Babolat Pure Drive"
+    },
+
+        {
+        name: "Babolot Pure Strike",
+        price: 59,
+        inStock: true,
+        brand: "Babolot",
+        src : "https://tse3.mm.bing.net/th/id/OIP.xzRQubpnNOIpMbKdyBxbjwAAAA?r=0&rs=1&pid=ImgDetMain&o=7&rm=3",
+        alt : "Babolat Pure Drive"
+    },
+
+        {
+        name: "Babolot Pure Strike",
+        price: 59,
+        inStock: true,
+        brand: "Babolot",
+        src : "https://tse3.mm.bing.net/th/id/OIP.xzRQubpnNOIpMbKdyBxbjwAAAA?r=0&rs=1&pid=ImgDetMain&o=7&rm=3",
+        alt : "Babolat Pure Drive"
+    },
+
+
+        {
+        name: "Babolot Pure Strike",
+        price: 59,
+        inStock: true,
+        brand: "Babolot",
+        src : "https://tse3.mm.bing.net/th/id/OIP.xzRQubpnNOIpMbKdyBxbjwAAAA?r=0&rs=1&pid=ImgDetMain&o=7&rm=3",
+        alt : "Babolat Pure Drive"
+    },
+
+        {
+        name: "Babolot Pure Strike",
+        price: 59,
+        inStock: true,
+        brand: "Babolot",
+        src : "https://tse3.mm.bing.net/th/id/OIP.xzRQubpnNOIpMbKdyBxbjwAAAA?r=0&rs=1&pid=ImgDetMain&o=7&rm=3",
+        alt : "Babolat Pure Drive"
+    },
+
+        {
+        name: "Babolot Pure Strike",
+        price: 59,
+        inStock: true,
+        brand: "Babolot",
+        src : "https://tse3.mm.bing.net/th/id/OIP.xzRQubpnNOIpMbKdyBxbjwAAAA?r=0&rs=1&pid=ImgDetMain&o=7&rm=3",
+        alt : "Babolat Pure Drive"
+    },
+
+        {
+        name: "Babolot Pure Strike",
+        price: 59,
+        inStock: true,
+        brand: "Babolot",
+        src : "https://tse3.mm.bing.net/th/id/OIP.xzRQubpnNOIpMbKdyBxbjwAAAA?r=0&rs=1&pid=ImgDetMain&o=7&rm=3",
+        alt : "Babolat Pure Drive"
+    },
+
+        {
+        name: "Babolot Pure Strike",
+        price: 59,
+        inStock: true,
+        brand: "Babolot",
+        src : "https://tse3.mm.bing.net/th/id/OIP.xzRQubpnNOIpMbKdyBxbjwAAAA?r=0&rs=1&pid=ImgDetMain&o=7&rm=3",
+        alt : "Babolat Pure Drive"
+    },
+
+        {
+        name: "Babolot Pure Strike",
+        price: 59,
+        inStock: true,
+        brand: "Babolot",
+        src : "https://tse3.mm.bing.net/th/id/OIP.xzRQubpnNOIpMbKdyBxbjwAAAA?r=0&rs=1&pid=ImgDetMain&o=7&rm=3",
+        alt : "Babolat Pure Drive"
+    },
+
 ];
 
 sneakers.forEach((rackets) => makeCard(rackets));
