ba8e7fdbc24d250ae198756733dee476312776d2
JoannaZhang-cs <jzsunset10@gmail.com>
2025-10-27 11:52:38 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,193 +1,211 @@
-
-
-
 const rackets = [
+  {
+    id: 1,
+    name: "Wilson Pro Staff",
+    price: 79,
+    inStock: true,
+    brand: "Wilson",
+    src: "https://www.wilson.com/en-us/tennis/tennis-rackets/media_1f64860413082850951fe50dfb684746385dc6863.jpg?width=750&format=jpg&optimize=medium",
+    alt: "Wilson Pro Staff",
+  },
 
-    {
-        name: "Wilson Pro Staff",
-        price: 79,
-        inStock: true,
-        brand: "Wilson",
-        src : "https://www.wilson.com/en-us/tennis/tennis-rackets/media_1f64860413082850951fe50dfb684746385dc6863.jpg?width=750&format=jpg&optimize=medium",
-        alt : "Wilson Pro Staff"
-    },
+  {
+    id: 1,
+    name: "Wilson Blade",
+    price: 49,
+    inStock: true,
+    brand: "Wilson",
+    src: "https://img.tennis-warehouse.com/watermark/rs.php?path=WBPRO6-1.jpg&nw=455",
+    alt: "Wilson Blade",
+  },
 
-        {
-        name: "Wilson Blade",
-        price: 49,
-        inStock: true,
-        brand: "Wilson",
-        src : "https://img.tennis-warehouse.com/watermark/rs.php?path=WBPRO6-1.jpg&nw=455",
-        alt : "Wilson Blade"
-    },
-    
-        {
-        name: "Wilson Clash",
-        price: 49,
-        inStock: true,
-        brand: "Wilson",
-        src : "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcSqSPlHT8Gj8GBXnwhEb4C89UZGRzWXoDWg9PF-YGtPIPi7BqLia9i3SXgOSvBDg3Hv7u8eoxcD7_-VqRSX1kaYKh-UlvgdtWEzGAlLrcxRihIjWp98czLKkw",
-        alt : "Wilson Clash"
-    },
+  {
+    id: 1,
+    name: "Wilson Clash",
+    price: 49,
+    inStock: true,
+    brand: "Wilson",
+    src: "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcSqSPlHT8Gj8GBXnwhEb4C89UZGRzWXoDWg9PF-YGtPIPi7BqLia9i3SXgOSvBDg3Hv7u8eoxcD7_-VqRSX1kaYKh-UlvgdtWEzGAlLrcxRihIjWp98czLKkw",
+    alt: "Wilson Clash",
+  },
 
-        {
-        name: "Wilson Ultra",
-        price: 49,
-        inStock: true,
-        brand: "Wilson",
-        src : "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSOpUq0s-DgDZNGj-p19GNiZl7YwuQg94YPR3kl05k3_XUY2kkDoHW4HTKeSPwUONo_evpomynvY72nDgoteJsSCS-Vawljm9AgBPJZbl1PXnf4YnCIFLlU",
-        alt : "Wilson Ultra"
-    },
+  {
+    id: 1,
+    name: "Wilson Ultra",
+    price: 49,
+    inStock: true,
+    brand: "Wilson",
+    src: "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSOpUq0s-DgDZNGj-p19GNiZl7YwuQg94YPR3kl05k3_XUY2kkDoHW4HTKeSPwUONo_evpomynvY72nDgoteJsSCS-Vawljm9AgBPJZbl1PXnf4YnCIFLlU",
+    alt: "Wilson Ultra",
+  },
 
-        {
-        name: "Babolat Pure Aero",
-        price: 59,
-        inStock: true,
-        brand: "Babolot",
-        src : "https://tennisexpress.com/cdn/shop/files/B101499-370-x.jpg?v=1752851049&width=1220",
-        alt : "Babolat Pure Aero"
-    },
+  {
+    id: 1,
+    name: "Babolat Pure Aero",
+    price: 59,
+    inStock: true,
+    brand: "Babolot",
+    src: "https://tennisexpress.com/cdn/shop/files/B101499-370-x.jpg?v=1752851049&width=1220",
+    alt: "Babolat Pure Aero",
+  },
 
-        {
-        name: "Babolat Pure Drive",
-        price: 59,
-        inStock: true,
-        brand: "Babolot",
-        src : "https://tse3.mm.bing.net/th/id/OIP.xzRQubpnNOIpMbKdyBxbjwAAAA?r=0&rs=1&pid=ImgDetMain&o=7&rm=3",
-        alt : "Babolat Pure Drive"
-    },
+  {
+    id: 1,
+    name: "Babolat Pure Drive",
+    price: 59,
+    inStock: true,
+    brand: "Babolot",
+    src: "https://tse3.mm.bing.net/th/id/OIP.xzRQubpnNOIpMbKdyBxbjwAAAA?r=0&rs=1&pid=ImgDetMain&o=7&rm=3",
+    alt: "Babolat Pure Drive",
+  },
 
-        {
-        name: "Babolot Pure Strike",
-        price: 59,
-        inStock: true,
-        brand: "Babolot",
-        src : "https://img.tennis-warehouse.com/watermark/rs.php?path=PS1620-1.jpg&nw=455",
-        alt : "Babolat Pure Strike"
-    },
+  {
+    id: 1,
+    name: "Babolot Pure Strike",
+    price: 59,
+    inStock: true,
+    brand: "Babolot",
+    src: "https://img.tennis-warehouse.com/watermark/rs.php?path=PS1620-1.jpg&nw=455",
+    alt: "Babolat Pure Strike",
+  },
 
-        {
-        name: "Babolot Boost",
-        price: 39,
-        inStock: true,
-        brand: "Babolot",
-        src : "https://img.tennis-warehouse.com/watermark/rs.php?path=BABOAP-1.jpg&nw=455",
-        alt : "Babolat Boost"
-    },
+  {
+    id: 1,
+    name: "Babolot Boost",
+    price: 39,
+    inStock: true,
+    brand: "Babolot",
+    src: "https://img.tennis-warehouse.com/watermark/rs.php?path=BABOAP-1.jpg&nw=455",
+    alt: "Babolat Boost",
+  },
 
-        {
-        name: "Yonex Ezone",
-        price: 59,
-        inStock: true,
-        brand: "Yonex",
-        src : "https://tse3.mm.bing.net/th/id/OIP.xzRQubpnNOIpMbKdyBxbjwAAAA?r=0&rs=1&pid=ImgDetMain&o=7&rm=3",
-        alt : "Yonex Ezone"
-    },
+  {
+    id: 1,
+    name: "Yonex Ezone",
+    price: 59,
+    inStock: true,
+    brand: "Yonex",
+    src: "https://tse3.mm.bing.net/th/id/OIP.xzRQubpnNOIpMbKdyBxbjwAAAA?r=0&rs=1&pid=ImgDetMain&o=7&rm=3",
+    alt: "Yonex Ezone",
+  },
 
-        {
-        name: "Yonex VCore",
-        price: 59,
-        inStock: true,
-        brand: "Yonex",
-        src : "https://img.tennis-warehouse.com/watermark/rs.php?path=VCR100-1.jpg&nw=455",
-        alt : "Yonex Vcore"
-    },
+  {
+    id: 1,
+    name: "Yonex VCore",
+    price: 59,
+    inStock: true,
+    brand: "Yonex",
+    src: "https://img.tennis-warehouse.com/watermark/rs.php?path=VCR100-1.jpg&nw=455",
+    alt: "Yonex Vcore",
+  },
 
-        {
-        name: "Yonex Astrel",
-        price: 49,
-        inStock: true,
-        brand: "Yonex",
-        src : "https://thetennisshoponline.com/wp-content/uploads/2024/01/yonex-astrel-105-web.jpg",
-        alt : "Yonex Astrel"
-    },
+  {
+    id: 1,
+    name: "Yonex Astrel",
+    price: 49,
+    inStock: true,
+    brand: "Yonex",
+    src: "https://thetennisshoponline.com/wp-content/uploads/2024/01/yonex-astrel-105-web.jpg",
+    alt: "Yonex Astrel",
+  },
 
-        {
-        name: "Yonex Percept",
-        price: 49,
-        inStock: true,
-        brand: "Yonex",
-        src : "https://img.tennis-warehouse.com/watermark/rs.php?path=PERC-1.jpg&nw=540",
-        alt : "Yonex Percept"
-    },
+  {
+    id: 1,
+    name: "Yonex Percept",
+    price: 49,
+    inStock: true,
+    brand: "Yonex",
+    src: "https://img.tennis-warehouse.com/watermark/rs.php?path=PERC-1.jpg&nw=540",
+    alt: "Yonex Percept",
+  },
 
-        {
-        name: "Dunlop FX 500",
-        price: 49,
-        inStock: true,
-        brand: "Dunlop",
-        src : "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcSN7_q99lcvOBgmZf6PiicAka-V9sVDta9-PkJ5PuCbgZMmXswRqsr7Z_vW9EKUIMYrODntlX6nZry0rfFXEGl_vb4DdaeebJmNrLrb6JuDrDOHDwzUBxeJ",
-        alt : "Dunlop FX 500"
-    },
+  {
+    id: 1,
+    name: "Dunlop FX 500",
+    price: 49,
+    inStock: true,
+    brand: "Dunlop",
+    src: "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcSN7_q99lcvOBgmZf6PiicAka-V9sVDta9-PkJ5PuCbgZMmXswRqsr7Z_vW9EKUIMYrODntlX6nZry0rfFXEGl_vb4DdaeebJmNrLrb6JuDrDOHDwzUBxeJ",
+    alt: "Dunlop FX 500",
+  },
 
-            {
-        name: "Dunlop SX 300",
-        price: 49,
-        inStock: true,
-        brand: "Dunlop",
-        src : "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcQ9MEwbmzHPuc4NyWEpNaIBgjRjIurEI75SYDcEemIOmCAPIaDYdTXV8eE-TEJ7E-DLQOXA53Cayw1fC_EPBmmQrE1vNboPF1M-mRp67fihI3VjzKcuTfssaw",
-        alt : "Dunlop SX 300"
-    },
+  {
+    id: 1,
+    name: "Dunlop SX 300",
+    price: 49,
+    inStock: true,
+    brand: "Dunlop",
+    src: "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcQ9MEwbmzHPuc4NyWEpNaIBgjRjIurEI75SYDcEemIOmCAPIaDYdTXV8eE-TEJ7E-DLQOXA53Cayw1fC_EPBmmQrE1vNboPF1M-mRp67fihI3VjzKcuTfssaw",
+    alt: "Dunlop SX 300",
+  },
 
-            {
-        name: "Dunlop CX 400",
-        price: 49,
-        inStock: true,
-        brand: "Dunlop",
-        src : "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcSeVuePyp6K1m1badCq-Oaa7wq2Rq-J6c9smvBzhDvjHu9qvznNNqiWpTxhmSrPfLXJvXY5us3J9jYxUo2rtU3Roi37t785cTzpFE49L7IlsmEi7yBBdUfZbg",
-        alt : "Dunlop FX 500"
-    },
+  {
+    id: 1,
+    name: "Dunlop CX 400",
+    price: 49,
+    inStock: true,
+    brand: "Dunlop",
+    src: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcSeVuePyp6K1m1badCq-Oaa7wq2Rq-J6c9smvBzhDvjHu9qvznNNqiWpTxhmSrPfLXJvXY5us3J9jYxUo2rtU3Roi37t785cTzpFE49L7IlsmEi7yBBdUfZbg",
+    alt: "Dunlop FX 500",
+  },
 
-            {
-        name: "Dunlop LX 1000",
-        price: 49,
-        inStock: true,
-        brand: "Dunlop",
-        src : "https://img.tennis-warehouse.com/watermark/rs.php?path=LX1000-1.jpg&nw=455",
-        alt : "Dunlop LX 1000"
-    },
+  {
+    id: 1,
+    name: "Dunlop LX 1000",
+    price: 49,
+    inStock: true,
+    brand: "Dunlop",
+    src: "https://img.tennis-warehouse.com/watermark/rs.php?path=LX1000-1.jpg&nw=455",
+    alt: "Dunlop LX 1000",
+  },
 
-            {
-        name: "Head Speed",
-        price: 69,
-        inStock: true,
-        brand: "Head",
-        src : "https://img.tennis-warehouse.com/watermark/rs.php?path=HSPDML-1.jpg&nw=455",
-        alt : "Head Speed"
-    },
-            {
-        name: "Head Extreme",
-        price: 49,
-        inStock: true,
-        brand: "Head",
-        src : "https://img.tennis-warehouse.com/watermark/rs.php?path=HREM24-1.jpg&nw=455",
-        alt : "Head Extreme"
-    },
-            {
-        name: "Head Radical",
-        price: 49,
-        inStock: true,
-        brand: "Head",
-        src : "https://img.tennis-warehouse.com/watermark/rs.php?path=XCEEDR-1.jpg&nw=455",
-        alt : "Head Radical"
-    },
-            {
-        name: "Head Gravity",
-        price: 49,
-        inStock: true,
-        brand: "Head",
-        src : "https://img.tennis-warehouse.com/watermark/rs.php?path=HGMPG-1.jpg&nw=455",
-        alt : "Head Gravity"
-    },
+  {
+    id: 1,
+    name: "Head Speed",
+    price: 69,
+    inStock: true,
+    brand: "Head",
+    src: "https://img.tennis-warehouse.com/watermark/rs.php?path=HSPDML-1.jpg&nw=455",
+    alt: "Head Speed",
+  },
+  {
+    id: 1,
+    name: "Head Extreme",
+    price: 49,
+    inStock: true,
+    brand: "Head",
+    src: "https://img.tennis-warehouse.com/watermark/rs.php?path=HREM24-1.jpg&nw=455",
+    alt: "Head Extreme",
+  },
+  {
+    id: 1,
+    name: "Head Radical",
+    price: 49,
+    inStock: true,
+    brand: "Head",
+    src: "https://img.tennis-warehouse.com/watermark/rs.php?path=XCEEDR-1.jpg&nw=455",
+    alt: "Head Radical",
+  },
+  {
+    id: 1,
+    name: "Head Gravity",
+    price: 49,
+    inStock: true,
+    brand: "Head",
+    src: "https://img.tennis-warehouse.com/watermark/rs.php?path=HGMPG-1.jpg&nw=455",
+    alt: "Head Gravity",
+  },
 ];
 
 rackets.forEach((rackets) => inject(rackets));
 
 //create inject function
-function inject(racket){
-    //do something
-    const container = document.querySelector(".container");
-    container.insertAdjacentHTML("afterbegin", `<div class="card">
+function inject(racket) {
+  //do something
+  const container = document.querySelector(".container");
+  container.insertAdjacentHTML(
+    "afterbegin",
+    `<div class="card">
         <img
           class="card-img"
           src= ${racket.src}
@@ -197,53 +215,50 @@ function inject(racket){
         <h5 class="price">$${racket.price}</h5>
           <button class="buy-btn">Buy Now</button>
         </a>
-      </div> `)
-      
-    //   const html = `<div class ="card" data-title=${item.name}` >
+      </div> `
+  );
 
+  //   const html = `<div class ="card" data-title=${item.name}` >
 
-    //query the container
-    //using adjacent html push card into container
+  //query the container
+  //using adjacent html push card into container
 }
-rackets.forEach((racket)=> inject(racket));
+rackets.forEach((racket) => inject(racket));
 //loop through items
 
-
-
-
-
 // 1) fix inject to include data-type and data-id, and use proper template quoting
-function inject(racket){
-    const container = document.querySelector(".container");
-    container.insertAdjacentHTML("beforeend", `
+function inject(racket) {
+  const container = document.querySelector(".container");
+  container.insertAdjacentHTML(
+    "beforeend",
+    `
         <div class="card" data-type="${racket.brand}" data-id="${racket.name}">
             <img class="card-img" src="${racket.src}" alt="${racket.alt}" />
             <h3 class="card-title">${racket.name}</h3>
             <h5 class="price">$${racket.price}</h5>
             <button class="buy-btn">Buy Now</button>
         </div>
-    `);
+    `
+  );
 }
 
 // render cards (call once)
-rackets.forEach(racket => inject(racket));
+rackets.forEach((racket) => inject(racket));
 
+function filterByGenre(brand) {
+  const cards = document.querySelectorAll(".card");
 
-function filterByGenre (brand) {
-    const cards = document.querySelectorAll(".card")
-
-    cards.forEach((card)=> {
-        const cardCategory = card.getAttribute("data-type");
-        console.log(cardCategory)
-        if (brand === cardCategory) {
-            card.style.display = ""; //contextual: could be ""(string), "block", "flex"
-        } else {
-            card.style.display = "none";
-
-        }
-    });
+  cards.forEach((card) => {
+    const cardCategory = card.getAttribute("data-type");
+    console.log(cardCategory);
+    if (brand === cardCategory) {
+      card.style.display = ""; //contextual: could be ""(string), "block", "flex"
+    } else {
+      card.style.display = "none";
+    }
+  });
 }
-filterByGenre("Yonex")
+filterByGenre("Yonex");
 // 3) create filter buttons (dynamically) and wire up clicks
 
 //     // unique types (brands)
@@ -273,10 +288,6 @@ filterByGenre("Yonex")
 
 // call after cards are rendered
 
-
-
-
-
 // function addToCart() {
 //     const buttons = document.querySelectorAll("add to cart")
 //     const btnArray = Array.from(buttons);
@@ -290,11 +301,11 @@ filterByGenre("Yonex")
 // //closest to find out the name of the card
 //         })
 
-//     ); 
+//     );
 //     //find the items in the array
 //     //take that object and pushes into cart
 
-// }                                                                                                                      
+// }
 // addToCart();
 
 // function addToCart(){
@@ -313,7 +324,7 @@ filterByGenre("Yonex")
 //             }
 //         })
 
-//     ); 
+//     );
 //     //find the items in the array
 //     //take that object and pushes into cart
 
@@ -325,33 +336,33 @@ filterByGenre("Yonex")
 //work on add to cart
 
 let prod = {
-        name: "Head Speed",
-        price: 69,
-        inStock: true,
-        brand: "Head",
-        src : "https://img.tennis-warehouse.com/watermark/rs.php?path=HSPDML-1.jpg&nw=455",
-        alt : "Head Speed"
-    }
+  name: "Head Speed",
+  price: 69,
+  inStock: true,
+  brand: "Head",
+  src: "https://img.tennis-warehouse.com/watermark/rs.php?path=HSPDML-1.jpg&nw=455",
+  alt: "Head Speed",
+};
 
 const cart = [];
-function createCartObject(product){
-    const cartProduct = {...product, quantity: 1};
-    return cartProduct;
-    //... is "spread operator". copies all elements of the proudct without rewriting it
+function createCartObject(product) {
+  const cartProduct = { ...product, quantity: 1 };
+  return cartProduct;
+  //... is "spread operator". copies all elements of the proudct without rewriting it
 }
 
-function checkCart(prod){
-    if (!prod) {
-        return;
-    }
-    //create cart object here
-    const cartProduct = {...prod, quantity: 1};
-    const found = cart.find((cartItem) => cartItem === cartProduct.title);
-    if(found){
-        found.quantity += 1
-    } else {
-        cart.push(prod);
-    }
+function checkCart(prod) {
+  if (!prod) {
+    return;
+  }
+  //create cart object here
+  const cartProduct = { ...prod, quantity: 1 };
+  const found = cart.find((cartItem) => cartItem === cartProduct.title);
+  if (found) {
+    found.quantity += 1;
+  } else {
+    cart.push(prod);
+  }
 }
 checkCart(prod);
 checkCart(prod);
